b53d8b58f4217822999e45a77fc75d36d00e0381
RBrunoChV <edu.rz@hotmail.com>
2022-07-18 19:51:36 -0500

    a

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 require("./db");
 //Middlewares
 app.use(cors());
-app.use('/static', express.static(path.join(__dirname, 'assets')))
+// app.use('/static', express.static(path.join(__dirname, 'assets')))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true }));
 // app.use(express.json());
